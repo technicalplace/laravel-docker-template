74907916be7065c6d0e7627aee670d5149fba998
technicalplace <shun26497204@gmail.com>
2024-11-10 23:10:08 +0900

    名前付きルートにリファクタ

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 });
 
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create');
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
