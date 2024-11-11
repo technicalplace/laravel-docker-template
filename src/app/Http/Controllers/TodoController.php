ff3d80cacfedba5e6ce82139abb6659844104cd4
technicalplace <shun26497204@gmail.com>
2024-11-11 09:50:40 +0900

    add: allとfillメソッドを使用し入力値を連想配列で受け取り一括代入、脆弱性回避のためfillableを定義

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -20,14 +20,16 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
+    /** todo新規作成処理 */
     public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all();
+        dd($inputs);
 
         // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
         $todo = new Todo();
         // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->content = $content;
+        $todo->fill($inputs);
         // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
         $todo->save();
 
